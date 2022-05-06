a189f4ce3904c5c1ec752b5e32c96796ad0eb7ac
Wallace Martins <wallace159santos@hotmail.com>
2022-05-06 08:42:03 -0300

    First commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "532d933ccc237e",
+    pass: "9b1a191507ac3b"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Wallace Martins <wallace159santos@hotmail.com>"  ,
+      subject,
+      html: body,
+    })
+  };
+}
